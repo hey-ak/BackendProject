4f1280b38862f34689e96460f647f327fafbfc47
hey-ak <akshay0127.be21@chitkara.edu.in>
2024-08-08 10:55:08 +0530

    fixed errors

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,21 +1,21 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controler.js";
-import upload from "../middlewares/multer.middleware.js"
+import { upload } from "../middlewares/multer.middleware.js";
+
 const router = Router();
 
 router.route("/register").post(
-    upload.fileds([
+    upload.fields([
         {
-            name:"avatar",
-            maxcount:1
+            name: "avatar",
+            maxCount: 1
         },
         {
-            name:"coverImage",
-            maxcount:1,
+            name: "coverImage",
+            maxCount: 1
         }
     ]),
-    registerUser)
-
-
+    registerUser
+);
 
 export default router;
